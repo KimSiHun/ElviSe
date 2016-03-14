c15cc0b645a8cd4fde3bb0f14e1f3bfd50d029cc
yun <liuj052@gmail.com>
2016-03-14 17:33:14 +0900

    open Qr Camera

--- Android/myprecious/MyPrecious/app/src/main/java/com/project/myprecious/elvis/myprecious/EnterQrPage.java
+++ Android/myprecious/MyPrecious/app/src/main/java/com/project/myprecious/elvis/myprecious/EnterQrPage.java
@@ -1,13 +1,35 @@
 package com.project.myprecious.elvis.myprecious;
 
 import android.app.Activity;
+import android.graphics.PointF;
 import android.os.Bundle;
 
-public class EnterQrPage extends Activity {
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+public class EnterQrPage extends Activity implements QRCodeReaderView.OnQRCodeReadListener {
+    private QRCodeReaderView qr_code_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_enterqr);
+        qr_code_view=(QRCodeReaderView) findViewById(R.id.qr_code_view);
+        qr_code_view.setOnQRCodeReadListener(this);
     }
+
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+
+    }
+
+    @Override
+    public void cameraNotFound() {
+
+    }
+
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+
+    }
+
 }
