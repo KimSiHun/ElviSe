3ab9f93ca40532ed18ff769bdc70337b516480f8
Woochan <twoseam61@gmail.com>
2016-03-14 17:15:15 +0900

    IndexController수정 및 indexPage생성

--- Server/MyPrecious/src/main/java/com/elvis/myprecious/IndexController.java
+++ Server/MyPrecious/src/main/java/com/elvis/myprecious/IndexController.java
@@ -1,18 +1,21 @@
 package com.elvis.myprecious;
 
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@RequestMapping(value = "/")
 @Controller
-@RequestMapping(value="/")
 public class IndexController {
 
-	@RequestMapping(method=RequestMethod.GET)
-	@ResponseBody
-	public String home() {
-		//¤·¤·
-		return "";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		return "index";
 	}
+
 }
