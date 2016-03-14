c8af479394ce8e68b58cbb5a0292f58ddb0676e4
yun <liuj052@gmail.com>
2016-03-14 15:51:17 +0900

    임시버튼 만들어서 다음페이지로 넘어가게 구성

--- Android/myprecious/MyPrecious/app/src/main/java/com/project/myprecious/elvis/myprecious/MainActivity.java
+++ Android/myprecious/MyPrecious/app/src/main/java/com/project/myprecious/elvis/myprecious/MainActivity.java
@@ -1,7 +1,10 @@
 package com.project.myprecious.elvis.myprecious;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO, 임시버튼임. QR 찍으면 다음페이지로 넘어가게 구성
+        Button goBtn1 = (Button)findViewById(R.id.goSangmin);
+        goBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PreciousOpen.class);
+                startActivity(intent);
+            }
+        });
+
+        //TODO, 임시버튼2
+        Button goBtn2 = (Button)findViewById(R.id.goGoohyun);
+        goBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,PreciousResult.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Android/myprecious/MyPrecious/app/src/main/java/com/project/myprecious/elvis/myprecious/PreciousOpen.java
@@ -0,0 +1,15 @@
+package com.project.myprecious.elvis.myprecious;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Yunjeong on 2016-03-14.
+ */
+public class PreciousOpen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_precious_open);
+    }
+}
